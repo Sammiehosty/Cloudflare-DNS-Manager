d58ed194a4ce96736283955af672944959cd4f25
Sammiehosty <samuelfrom1991@gmail.com>
2026-06-02 13:52:44 +0100

    Increase max height of clients list from 300px to 500px

--- src/components/ClientsList.tsx
+++ src/components/ClientsList.tsx
@@ -371,7 +371,7 @@ export const ClientsList: React.FC<Props> = ({
       )}
 
       {/* Clients List */}
-      <div className="p-3 max-h-[300px] overflow-y-auto">
+      <div className="p-3 max-h-[500px] overflow-y-auto">
         {loading ? (
           <div className="flex justify-center py-6">
             <LoadingSpinner size="lg" text="Loading clients..." />
